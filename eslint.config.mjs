28581209e9b29467225d5224208e7ec284b8d69a
ederbiason <ederbiason@hotmail.com>
2025-02-12 16:09:32 -0300

    update: disable eslint config for key prop erros

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+    rules: {
+      'react/jsx-key': [1, { checkFragmentShorthand: true }]
+    }
+  }),
+]
 
 export default eslintConfig;
